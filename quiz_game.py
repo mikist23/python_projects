f30fc864932415f771ef972a9634bc60bc4ca359
@Mikist <mikeochango3@gmail.com>
2024-07-04 23:30:09 +0300

    calculating scores

--- quiz_game.py
+++ quiz_game.py
@@ -2,36 +2,44 @@ print("Welcome to the Game! ")
 
 playing = input("Do you want to play the game! ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Let's play the Game! ")
-
+score  =0
 
 
 answer = input("What does CPU mean in computig ? ") 
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct! ")
+    score  += 1
 else:
     print("Incorrect! ")  
 
 answer = input("What does GPU stand for ? ") 
-if answer == "graphics processing unit":
+if answer.lower() == "graphics processing unit":
     print("Correct! ")
+    score  += 1
 else:
     print("Incorrect! ")          
 
 
 answer = input("What does PSU stand for ?") 
-if answer == "power supply unit":
+if answer.lower() == "power supply unit":
     print("Correct! ")
+    score  += 1
 else:
     print("Incorrect! ")  
 
 answer = input("What is Python in computing ? ") 
-if answer == "programming language":
+if answer.lower() == "programming language":
     print("Correct! ")
+    score  += 1
 else:
     print("Incorrect! ")  
+
+
+print("You Got " + str(score) + "Correct")
+print("Your score is " + str((score/4) *100) + "%")    
 
 
