ffc0605b131dd9d31bc4d5ad7255fc08845992fe
@Mikist <mikeochango3@gmail.com>
2024-07-04 23:44:27 +0300

    update input logic

--- number_guess.py
+++ number_guess.py
@@ -2,3 +2,15 @@ print("Welcome to the Game! ")
 
 import random
 
+top_range = input("Type a number ? ")
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+    if top_range <= 0 :
+        print("Plesase type a number larger than zero next time")
+        quit()
+
+else:
+    print("Plesase type a number next time")
+    quit()
