efec1b2fdf4ecbd1b0cd6bc47202e389f2c878fa
@Mikist <mikeochango3@gmail.com>
2024-07-04 23:09:39 +0300

    Added more questions and answers

--- quiz_game.py
+++ quiz_game.py
@@ -7,8 +7,31 @@ if playing != "yes":
 
 print("Let's play the Game! ")
 
+
+
 answer = input("What does CPU mean in computig ? ") 
 if answer == "central processing unit":
     print("Correct! ")
 else:
-    print("Incorrect! ")       
+    print("Incorrect! ")  
+
+answer = input("What does GPU stand for ? ") 
+if answer == "graphics processing unit":
+    print("Correct! ")
+else:
+    print("Incorrect! ")          
+
+
+answer = input("What does PSU stand for ?") 
+if answer == "power supply unit":
+    print("Correct! ")
+else:
+    print("Incorrect! ")  
+
+answer = input("What is Python in computing ? ") 
+if answer == "programming language":
+    print("Correct! ")
+else:
+    print("Incorrect! ")  
+
+
