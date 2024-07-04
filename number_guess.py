601f3afbc597387889ca4c315bbc1578d695eb43
@Mikist <mikeochango3@gmail.com>
2024-07-04 23:57:40 +0300

    updated the logic

--- number_guess.py
+++ number_guess.py
@@ -14,3 +14,23 @@ if top_range.isdigit():
 else:
     print("Plesase type a number next time")
     quit()
+random_number = random.randint(0, top_range)
+
+guesses = 0
+while True:
+    guesses +=1
+    user_num = input("Make a guess ? ")
+    if user_num.isdigit():
+        user_num= int(user_num)
+    else:
+        print("Enter a number next time!!! ")
+        continue  
+
+
+    if user_num == random_number:
+        print("You got it !!") 
+        break
+    else:
+        print("Wrong!!!!")     
+
+print("You got it after ", guesses, "guesses")
