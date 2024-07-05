1a26c8ba9d9e61bec6e6e690452ad1d4b667b5f2
@Mikist <mikeochango3@gmail.com>
2024-07-05 22:38:42 +0300

    Take in user inputs

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,3 +6,11 @@ user_wins = 0
 computer_wins = 0
 
 options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Enter either Rock, Paper or Scissors or Q to quit! ").lower()
+    if user_input.lower() == "q":
+        break
+
+    if user_input not in options:
+         continue
