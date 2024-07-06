0831707fd83176de13e288a550c49cd00f72746f
@Mikist <mikeochango3@gmail.com>
2024-07-06 22:49:28 +0300

    Define the user prompt

--- password-manager.py
+++ password-manager.py
@@ -8,3 +8,18 @@ def add():
 def view():
      pass
 
+while True:
+     mode = input("Do you want to view or add password (view,add) or q to quit").lower()
+
+     if mode == "q":
+          break
+     
+     if mode == "view":
+          view()
+
+     elif mode == add():
+          add()
+
+     else:
+        print("Enter valid option!!!")
+        continue
