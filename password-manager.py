e21a00cfa102eb903ee131f8b279ebbf54d5d86c
@Mikist <mikeochango3@gmail.com>
2024-07-06 23:01:55 +0300

    adedd the add function logic

--- password-manager.py
+++ password-manager.py
@@ -1,15 +1,19 @@
 print("Welcome to the game!! ")
-pwd = input("Enter your master password? ")
+master_pwd = input("Enter your master password? ")
 
 
 def add():
-     pass
+     name = input("Enter your name? ")
+     pwd = input("Enter your password? ")
+
+     with open("password.txt", "a") as f:
+          f.write(name + "|" + pwd)
 
 def view():
      pass
 
 while True:
-     mode = input("Do you want to view or add password (view,add) or q to quit").lower()
+     mode = input("Do you want to view or add password (view,add) or q to quit? ").lower()
 
      if mode == "q":
           break
