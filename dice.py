b8ab66a084a1ceb818d77395ab77dde564d8f83d
@Mikist <mikeochango3@gmail.com>
2024-07-08 00:09:59 +0300

    No of layers logic

--- dice.py
+++ dice.py
@@ -5,7 +5,18 @@ def roll():
     min_value = 1
     max_value = 6
 
-    value = random.randint(min_value,max_value)
-    return value
+    roll = random.randint(min_value,max_value)
+    return roll
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+       players = int(players)
+       if 2 <= players <=4:
+           break
+       else:
+           print("Must be between 2 - 4 players")
+           
+    else:
+      print("Invalid input!!!")  
 
-print(roll())
+print(players)           
