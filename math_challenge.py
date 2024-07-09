1b09a58e65669e5a1f5cc5d610176f6e03230290
@Mikist <mikeochango3@gmail.com>
2024-07-10 00:09:42 +0300

    finalised the project

--- math_challenge.py
+++ math_challenge.py
@@ -1,5 +1,6 @@
 print("Welcome to the game!!! ")
 import random
+import time
 
 OPERAORS = ["+","-","*"]
 MIN_OPERAND = 3
@@ -15,10 +16,19 @@ def generate_problem():
     answer = eval(expr)
     # print(expr, answer)
     return expr, answer
-    
+wrong = 0
+input('Press enter to start ')
+print("------------Start----------------")
+start_time = time.time()
 for i in range(TOTAL_PROBLEMS):
     expr , answer = generate_problem()
     while True:
         guess = input("Problem # "+ str(i+1)+ ": " + expr + " = ")
         if guess == str(answer):
+            wrong += 1
             break
+print("------------End----------------")  
+end_time = time.time()  
+total_time = time.ctime(end_time - start_time)
+print(f"You finished in {end_time :.2}")      
+      
