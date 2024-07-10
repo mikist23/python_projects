5313ed3e37700cf91328418c7341b3a35593cd69
@Mikist <mikeochango3@gmail.com>
2024-07-10 23:26:13 +0300

    check user amount

--- slot_machine.py
+++ slot_machine.py
@@ -1,4 +1,15 @@
 print("Welcome to the game!!!")
 def deposit():
-    pass
+    while True:
+        amount = input("Enter ammount you want to deposit: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greter than 0")
+
+        else:
+            print("Please antr a number!!!! ") 
+    return amount
 deposit()              
