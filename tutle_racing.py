67101420b03a02fb3b94df101c263bb31b9397c0
@Mikist <mikeochango3@gmail.com>
2024-07-12 23:11:49 +0300

    randomly  select the colors

--- tutle_racing.py
+++ tutle_racing.py
@@ -1,12 +1,8 @@
 import turtle
+import random
 WIDTH , HEIGHT = 500, 500
 
-screen = turtle.Screen().setup(WIDTH, HEIGHT)
-screen = turtle.title("Turtle Racing Game !! ")
-# screen.title("Turtle Racing Game! ")
-
-
-
+COLORS = ["red","green","blue","orange", "yellow", "black", "pink", "brown", "purple","cyan" ]
 
 
 def get_number_of_racers():
@@ -23,5 +19,12 @@ def get_number_of_racers():
             return racers
         else:
             print("Number not in range !!! Try again")
+
+def init_turtle():
+    screen = turtle.Screen().setup(WIDTH, HEIGHT)
+    screen = turtle.title("Turtle Racing Game !! ")
+    # screen.title("Turtle Racing Game! ")
+
 racers = get_number_of_racers()
-print(racers)
+init_turtle()
+random.shuffle(COLORS)
