9fb6eb2f6e2ad8b5fa8b17fa6ccce591afeb8149
@Mikist <mikeochango3@gmail.com>
2024-07-14 00:34:57 +0300

    difine the method to generate password

--- password_generator.py
+++ password_generator.py
@@ -19,12 +19,29 @@ def generate_password(min_length, numbers = True, special_characters = True):
     has_number = False
     has_special = False
 
-   
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+    return pwd   
 
                   
-
+min_length = int(input("Enter the minimum length: "))
     
 
 
 
-generate_password(10) 
+pwd = generate_password(10) 
+print(pwd)
