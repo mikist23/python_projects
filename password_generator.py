f9ec5267fa6e070d555935884974cc7cf195edef
@Mikist <mikeochango3@gmail.com>
2024-07-14 00:30:20 +0300

    initialize the parameters need

--- password_generator.py
+++ password_generator.py
@@ -5,11 +5,26 @@ import random
 
 def generate_password(min_length, numbers = True, special_characters = True):
     letters = string.ascii_letters
-    numbers = string.digits
+    digits = string.digits
     special= string.punctuation
 
-    print(letters , numbers , special)
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters == special   
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+   
+
+                  
+
+    
 
 
 
-generate_password(10)
+generate_password(10) 
