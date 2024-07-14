e0e37d1dda2347f64c1f9f3a1b0ff5d1804d4aef
@Mikist <mikeochango3@gmail.com>
2024-07-15 00:16:43 +0300

    difine the display size

--- snake_apple_game.py
+++ snake_apple_game.py
@@ -4,3 +4,9 @@ import time
 
 if __name__ == "__main__":
     pygame.init()
+
+    surface = pygame.display.set_mode((1000, 500))
+    surface.fill((255,255,255))
+    pygame.display.flip()
+
+    time.sleep(5)
