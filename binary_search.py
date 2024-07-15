d9478b129f9eb53ab83f5e989855985054368409
@Mikist <mikeochango3@gmail.com>
2024-07-15 23:25:59 +0300

    Initiated the method to check the midpoints

--- binary_search.py
+++ binary_search.py
@@ -18,6 +18,22 @@ def binary_search(a_list, an_item):
    return False
 
 
+def binary_search_rec(a_list, first, last, an_item):
+   if len(a_list) == 0:
+       return False
+   else:
+       mid_point = (first + last) // 2
+       if a_list[mid_point] == an_item:
+           return True
+       else:
+           if an_item < a_list[mid_point]:
+               last = mid_point - 1
+               return binary_search_rec(a_list, first, last, an_item)
+           else:
+               first = mid_point + 1
+               return binary_search_rec(a_list, first, last, an_item)
+
+
 
 if __name__ == '__main__':
    a_list = [1, 4, 7, 10, 14, 19, 102, 2575, 10000]
