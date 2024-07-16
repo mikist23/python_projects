7b5f6ab30c30585d4f3b2858205baefa7da60bc1
@Mikist <mikeochango3@gmail.com>
2024-07-16 23:30:37 +0300

    enable the movement of the block using key events

--- snake_apple_game.py
+++ snake_apple_game.py
@@ -5,6 +5,7 @@ from pygame.locals import *
 
 def draw_block():
     surface.fill((110,110,5))
+    
     surface.blit(block, (block_x,block_y))
     pygame.display.flip()
 
@@ -31,8 +32,20 @@ if __name__ == "__main__":
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                if event.type == K_ESCAPE:
+                if event.key == K_ESCAPE:
                     running = False
-               
+                if event.key == K_UP:
+                    block_y -= 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 10
+                    draw_block()
+
             elif event.type == QUIT:
                 running = False
