bdd6305be554cf90284724bc948c6c3e57400b4c
@Mikist <mikeochango3@gmail.com>
2024-07-16 23:15:45 +0300

    difine the draw_block function

--- snake_apple_game.py
+++ snake_apple_game.py
@@ -3,18 +3,36 @@ import time
 from pygame.locals import *
 
 
+def draw_block():
+    surface.fill((110,110,5))
+    surface.blit(block, (block_x,block_y))
+    pygame.display.flip()
+
+
+
 if __name__ == "__main__":
     pygame.init()
 
     surface = pygame.display.set_mode((1000, 500))
-    surface.fill((255,255,255))
+    surface.fill((110,110,5))
+    block  = pygame.image.load("resources/block.jpg").convert()
+    block_x = 100
+    block_y = 100
+    surface.blit(block, (block_x,block_y))
+
+
+
+
     pygame.display.flip()
+
 
     running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                pass
+                if event.type == K_ESCAPE:
+                    running = False
+               
             elif event.type == QUIT:
                 running = False
