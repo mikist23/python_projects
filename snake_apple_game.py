bbe31502079f9f0fd27fda05d02095395ed32ac0
@Mikist <mikeochango3@gmail.com>
2024-07-17 22:37:17 +0300

    difine a draw method

--- snake_apple_game.py
+++ snake_apple_game.py
@@ -8,6 +8,13 @@ class Snake:
         self.block  = pygame.image.load("resources/block.jpg").convert()
         self.x =100
         self.y =100
+
+    def draw(self):
+        self.surface.fill((110,110,5))
+        self.parent_screen.bilt(self.block, (self.x,self.y))
+        pygame.display.flip()
+
+
         
 
 
@@ -25,7 +32,7 @@ if __name__ == "__main__":
 
     surface = pygame.display.set_mode((1000, 500))
     surface.fill((110,110,5))
-    block  = pygame.image.load("resources/block.jpg").convert()
+    
     block_x = 100
     block_y = 100
     surface.blit(block, (block_x,block_y))
