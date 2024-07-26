f86515214e5797bf6d270f51c5b3efeb57ebab6d
@Mikist <mikeochango3@gmail.com>
2024-07-26 22:13:34 +0300

    Difine start bot function

--- auto_login_bot.py
+++ auto_login_bot.py
@@ -4,7 +4,28 @@ import os
  
 # Get the path of chromedriver which you have install
  
-
+def startBot(username, password, url):
+    pass
+#     path = "C:\\Users\\hp\\Downloads\\chromedriver"
+     
+#     # giving the path of chromedriver to selenium webdriver
+#     driver = webdriver.Chrome(path)
+     
+#     # opening the website  in chrome.
+#     driver.get(url)
+     
+#     # find the id or name or class of
+#     # username by inspecting on username input
+#     driver.find_element_by_name(
+#         "id/class/name of username").send_keys(username)
+     
+#     # find the password by inspecting on password input
+#     driver.find_element_by_name(
+#         "id/class/name of password").send_keys(password)
+     
+#     # click on submit
+#     driver.find_element_by_css_selector(
+#         "id/class/name/css selector of login button").click()
  
  
 # Driver Code
@@ -17,4 +38,4 @@ password = "Enter your password"
 url = "Enter the URL of login page of website"
  
 # Call the function
-# startBot(username, password, url)
+startBot(username, password, url)
