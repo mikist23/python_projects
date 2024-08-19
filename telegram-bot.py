62fd0de7a5744b95d03a55d16b1887eb4a42c2d1
@Mikist <mikeochango3@gmail.com>
2024-08-19 23:29:48 +0300

    start function

--- telegram-bot.py
+++ telegram-bot.py
@@ -4,3 +4,13 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler 
 from telegram.ext.messagehandler import MessageHandler 
 from telegram.ext.filters import Filters 
+
+
+updater = Updater("your_own_API_Token got from BotFather", 
+				use_context=True) 
+
+
+def start(update: Update, context: CallbackContext): 
+	update.message.reply_text( 
+		"Enter the text you want to show to the user whenever they start the bot") 
+
