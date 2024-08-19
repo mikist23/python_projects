371b579128def964af782f3d4c4af09beb26a06d
@Mikist <mikeochango3@gmail.com>
2024-08-19 23:30:50 +0300

    difine the functions

--- telegram-bot.py
+++ telegram-bot.py
@@ -16,3 +16,31 @@ def start(update: Update, context: CallbackContext):
 
 def help(update: Update, context: CallbackContext): 
 	update.message.reply_text("Your Message") 
+
+
+def gmail_url(update: Update, context: CallbackContext): 
+	update.message.reply_text("gmail link here") 
+
+
+def youtube_url(update: Update, context: CallbackContext): 
+	update.message.reply_text("youtube link") 
+
+
+def linkedIn_url(update: Update, context: CallbackContext): 
+	update.message.reply_text("Your linkedin profile url") 
+
+
+def geeks_url(update: Update, context: CallbackContext): 
+	update.message.reply_text("GeeksforGeeks url here") 
+
+
+def unknown_text(update: Update, context: CallbackContext): 
+	update.message.reply_text( 
+		"Sorry I can't recognize you , you said '%s'" % update.message.text) 
+
+
+def unknown(update: Update, context: CallbackContext): 
+	update.message.reply_text( 
+		"Sorry '%s' is not a valid command" % update.message.text) 
+
+
