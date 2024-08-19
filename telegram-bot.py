86742999e952ec34929e9e8ef0e95df3eefbea31
@Mikist <mikeochango3@gmail.com>
2024-08-19 23:32:13 +0300

    updater functions

--- telegram-bot.py
+++ telegram-bot.py
@@ -43,4 +43,16 @@ def unknown(update: Update, context: CallbackContext):
 	update.message.reply_text( 
 		"Sorry '%s' is not a valid command" % update.message.text) 
 
-
+updater.dispatcher.add_handler(CommandHandler('start', start)) 
+updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url)) 
+updater.dispatcher.add_handler(CommandHandler('help', help)) 
+updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url)) 
+updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url)) 
+updater.dispatcher.add_handler(CommandHandler('geeks', geeks_url)) 
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown)) 
+updater.dispatcher.add_handler(MessageHandler( 
+    # Filters out unknown commands 
+    Filters.command, unknown)) 
+  
+# Filters out unknown messages. 
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text)) 
